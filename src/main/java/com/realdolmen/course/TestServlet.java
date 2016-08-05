1879aa8bd7163d2e43c74623b537a9cfdde6e6fe
Vincent Ceulemans <vincent.ceulemans.120@gmail.com>
2016-08-05 17:04:34 +0200

    Add JSP Page with animals

--- src/main/java/com/realdolmen/course/TestServlet.java
+++ src/main/java/com/realdolmen/course/TestServlet.java
@@ -6,14 +6,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
 @WebServlet(urlPatterns = "/TestServlet")
 public class TestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter writer = response.getWriter();
-        writer.write("Hello World");
+        List<String> list = Arrays.asList(
+               "Dog", "Cat", "Horse", "Mouse", "Duck"
+        );
+
+        request.setAttribute("animals", list);
+        request.getRequestDispatcher("WEB-INF/test.jsp").forward(request,response);
     }
 }
--- src/main/webapp/WEB-INF/test.jsp
+++ src/main/webapp/WEB-INF/test.jsp
@@ -1,8 +1,17 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
+    <h1>JSP Page</h1>
+    <p>Hello World from JSP</p>
 
+    <h1>This is a list of animals</h1>
+    <ul>
+    <c:forEach items="${animals}" var="animal">
+        <li>${animal} </li>
+    </c:forEach>
+    </ul>
 </body>
 </html>
