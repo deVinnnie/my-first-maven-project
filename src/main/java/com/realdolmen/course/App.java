a56bb77052ded5b0ab59c87e2d4e3c06e3cba471
Vincent Ceulemans <vincent.ceulemans.120@gmail.com>
2016-08-05 14:54:25 +0200

    Add some maven plugins

--- src/main/java/com/realdolmen/course/App.java
+++ src/main/java/com/realdolmen/course/App.java
@@ -2,12 +2,36 @@ package com.realdolmen.course;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private int i;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        /*Runnable r = () -> {
+            System.out.println("Hello World");
+        };*/
+
+        try {
+            int i = 0;
+            for (i = 0; i < 10; i++) {
+                System.out.println("Hello" + "this" + "is" + i);
+            }
+        } catch (Exception e) {
+            System.out.println("Hello");
+        }
+    }
+
+    public App(int a){
+        this.i = calc(a);
+    }
+
+    private int calc(int a){
+        return a*5;
+    }
+
+    public int getI() {
+        return this.i;
     }
 }
